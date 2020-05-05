62273e65450c0a441eb45d7052fc407a2b90721a
kudumu <makusudi@gmail.com>
2020-05-05 12:23:04 +0300

    minor fix

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-DB_HOST = '127.0.0.1'
+import os
+
+DB_HOST = os.environ.get('DB_HOST')
 DB_USER = 'postgres'
 DB_PASS = 'postgres'
 CONNECTION_STRING = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/postgres'
